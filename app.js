8fc3f7c5c3324633792eec981c905cf3d39d2f00
kswarn <swarnakadagadkai1@gmail.com>
2021-12-16 09:30:43 +0530

    Added cors

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 
 require("./startup/logging")();
+require("./startup/cors")(app);
+
 require("./startup/routes")(app);
 require("./startup/database")();
 require("./startup/config")();
--- middleware/auth.js
+++ middleware/auth.js
@@ -2,17 +2,14 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 
 module.exports = function (req, res, next) {
-  const token = req.header("x-auth-token");
+  if (!config.get("requiresAuth")) return next();
 
+  const token = req.header("x-auth-token");
   if (!token) return res.status(401).send("Access denied. No token provided.");
 
   try {
-    const decodedPayload = jwt.verify(token, config.get("jwtPrivateKey"));
-
-    // attach the payload to the req obj, so it can be extracted in the route handlers
-
-    // req object is not accessible using supertest, so we write unit test for this
-    req.user = decodedPayload;
+    const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+    req.user = decoded;
     next();
   } catch (ex) {
     res.status(400).send("Invalid token.");
